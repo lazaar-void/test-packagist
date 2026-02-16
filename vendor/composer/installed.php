9c104869f58949c5d014f3d7d1254ceb465be3a3
hamza-lzr <lazaarhamza@gmail.com>
2026-02-16 17:41:31 +0000

    vendor files after composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/typing_php',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'fa49475824cd6bf107788efeb882b90c5251d016',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/typing_php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fa49475824cd6bf107788efeb882b90c5251d016',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
